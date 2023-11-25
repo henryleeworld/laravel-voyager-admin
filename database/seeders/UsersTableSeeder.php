343d6e110c59943e6111ff3fb6eb582b574618e2
Henry Lee <henryleeworld@gmail.com>
2023-11-26 00:14:35 +0800

    feat:更新最新資料

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -22,7 +22,7 @@ class UsersTableSeeder extends Seeder
             User::create([
                 'name'           => 'Admin',
                 'email'          => 'admin@admin.com',
-                'password'       => 'password',
+                'password'       => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
                 'remember_token' => Str::random(60),
                 'role_id'        => $role->id,
             ]);
